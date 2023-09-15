da82f499d964acee30ea316fc08d5339ef8451d2
andressa-m <127565852+andressa-m@users.noreply.github.com>
2023-09-15 19:54:28 -0300

    arquivos

--- style.cs
+++ style.cs
@@ -6,3 +6,34 @@
     top; 369px;
 background: url( image.png);
 }
+
+.img1{
+width:678px;
+height:319px;
+flex-shrink:0;
+}
+
+.img2{
+width:466px;
+heigth:303px;
+flex-shrink:0;
+}
+
+.img3{
+width:245px;
+height:321px;
+flex-shrink:0;   
+}
+
+.img4{
+ width:412px;
+ height:278px;
+ flex-shrink:0;   
+}
+
+.img5{
+width:462px;
+height:269px;
+flex-shrink:0;    
+}
+
