e8d26d00e02d8919966f30c71e5d6b2ce935c6f4
andressa-m <127565852+andressa-m@users.noreply.github.com>
2023-09-22 19:47:01 -0300

    estilo

--- style.cs
+++ style.cs
@@ -1,6 +1,4 @@
-body{
-    background:#921C1C
-}
+
 
 .cabecalho{
     position: absolute;
@@ -8,36 +6,35 @@ body{
     height: 303px;
     left: 860px;
     top; 369px;
-background: url( image.png);
 }
 
 .img1{
 width:678px;
 height:319px;
-flex-shrink:0;
+
 }
 
 .img2{
 width:466px;
 heigth:303px;
-flex-shrink:0;
+
 }
 
 .img3{
 width:245px;
 height:321px;
-flex-shrink:0;   
+   
 }
 
 .img4{
  width:412px;
  height:278px;
- flex-shrink:0;   
+   
 }
 
 .img5{
 width:462px;
 height:269px;
-flex-shrink:0;    
+  
 }
 
